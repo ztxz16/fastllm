4bb404dc4331acad837ea7a467e84506571086f0
huangyuyang <410644548@qq.com>
2023-05-26 23:51:20 +0800

    增加一些cuda函数

--- include/fastllm-cuda.h
+++ include/fastllm-cuda.h
@@ -5,17 +5,25 @@ extern "C" {
 #endif
 void FastllmInitCublas(void);
 
-bool FastllmMatMulFloatInt8(const fastllm::Data &input, fastllm::Data &weight, const fastllm::Data &bias, fastllm::Data &output, int n, int m, int k);
-
-bool FastllmMatMulFloatInt4(const fastllm::Data &input, fastllm::Data &weight, const fastllm::Data &bias, fastllm::Data &output, int n, int m, int k);
-
 void *FastllmCudaMalloc(size_t size);
+void FastllmCudaFree(void *ret);
 
 void FastllmCudaCopyFromHostToDevice(void *dst, void *src, size_t size);
 void FastllmCudaCopyFromDeviceToHost(void *dst, void *src, size_t size);
-void FastllmCudaFree(void *ret);
+void FastllmCudaCopyFromDeviceToDevice(void *dst, void *src, size_t size);
 
-bool FastllmGelu(const fastllm::Data &input, fastllm::Data &output);
+void FastllmCudaMemcpy2DDeviceToDevice(void * 	dst, size_t 	dpitch, const void * 	src,
+                                       size_t 	spitch, size_t 	width, size_t 	height);
+
+bool FastllmCudaGeluNew(const fastllm::Data &input, fastllm::Data &output);
+bool FastllmCudaMul(const fastllm::Data &input, float v, fastllm::Data &output);
+bool FastllmCudaSoftmax(const fastllm::Data &input, fastllm::Data &output, int axis);
+bool FastllmCudaMatMulFloatInt8(const fastllm::Data &input, fastllm::Data &weight, const fastllm::Data &bias, fastllm::Data &output, int n, int m, int k);
+bool FastllmCudaMatMulFloatInt4(const fastllm::Data &input, fastllm::Data &weight, const fastllm::Data &bias, fastllm::Data &output, int n, int m, int k);
+bool FastllmCudaBatchMatMulTransB(const fastllm::Data &input0, const fastllm::Data &input1, fastllm::Data &output,
+                              int input0Spatial, int input1Spatial, int outputSpatial,
+                              int input0Stride, int input1Stride,
+                              int batch, int n, int m, int k, float alpha);
 
 #ifdef  __cplusplus
 }
