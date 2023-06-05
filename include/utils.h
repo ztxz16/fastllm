fcd9d61836bdda1e8eafa95a14ad096b035a107e
yuyang huang <410644548@qq.com>
2023-06-06 01:10:37 +0800

    更新readme

--- include/utils.h
+++ include/utils.h
@@ -13,7 +13,7 @@
 #include <cstdint>
 
 namespace fastllm {
-    static uint as_uint(const float x) {
+    static uint32_t as_uint(const float x) {
         return *(uint32_t*)&x;
     }
     static float as_float(const uint32_t x) {
@@ -21,9 +21,9 @@ namespace fastllm {
     }
 
     static float half_to_float(const uint16_t x) { // IEEE-754 16-bit floating-point format (without infinity): 1-5-10, exp-15, +-131008.0, +-6.1035156E-5, +-5.9604645E-8, 3.311 digits
-        const uint e = (x & 0x7C00) >> 10; // exponent
-        const uint m = (x & 0x03FF) << 13; // mantissa
-        const uint v = as_uint((float) m) >> 23; // evil log2 bit hack to count leading zeros in denormalized format
+        const uint32_t e = (x & 0x7C00) >> 10; // exponent
+        const uint32_t m = (x & 0x03FF) << 13; // mantissa
+        const uint32_t v = as_uint((float) m) >> 23; // evil log2 bit hack to count leading zeros in denormalized format
         return as_float((x & 0x8000) << 16 | (e != 0) * ((e + 112) << 23 | m) | ((e == 0) & (m != 0)) * ((v - 37) << 23 |
                                                                                                          ((m << (150 - v)) &
                                                                                                           0x007FE000))); // sign : normalized : denormalized
