07eb64a7f2f591f2b44ce6bcf26c30267a0ceb38
huangyuyang <410644548@qq.com>
2023-06-25 19:09:54 +0800

    webui多线程支持

--- example/webui/webui.cpp
+++ example/webui/webui.cpp
@@ -10,70 +10,84 @@
 #include <thread>
 #include <stdlib.h>
 #include <string>
+#include <mutex>
 
-std::string GBKToUTF8(const std::string& strGBK);
+struct ChatSession {
+    std::string history = "";
+    std::string input = "";
+    std::string output = "";
+    int round = 0;
+    int status = 0; // 0: 空闲 1: 结果生成好了 2: 已经写回了
+};
+
+std::map <std::string, ChatSession*> sessions;
+std::mutex locker;
 
 int main(int argc, char** argv) {
     fastllm::SetThreads(8);
-    fastllm::ChatGLMModel chatGlm;
-
-    std::string type;
-    std::cout << "Use chatglm-6b-v1.1-int4 or chatglm-6b-v1.1-int8 ? 8/4 (Default = 4) ";
-    std::getline(std::cin, type);
-    if (type == "8") {
-        chatGlm.LoadFromFile("chatglm-6b-v1.1-int8.bin");
-    } else if (type == "4" || type == "") {
-        chatGlm.LoadFromFile("chatglm-6b-v1.1-int4.bin");
-    }
-
-    std::string history = "";
-    int round = 0;
-    static std::string ss = "";
+    fastllm::ChatGLMModel model;
+    model.LoadFromFile(argv[1]);
 
     httplib::Server svr;
-    std::atomic_bool waiting;
-    waiting = false;
-    std::string last_request = "";
 
-    auto chat = [&](std::string input) {
+    auto chat = [&](ChatSession *session, const std::string input) {
         if (input == "reset" || input == "stop") {
-            history = "";
-            round = 0;
-            ss = "<eop>\n";
+            session->history = "";
+            session->round = 0;
+            session->output = "<eop>\n";
+            session->status = 2;
         } else {
-            history += ("[Round " + std::to_string(round++) + "]\n问：" + input);
-            auto prompt = round > 1 ? history : input;
-
-            waiting = true;
-            std::string ret = chatGlm.Response(prompt, [](int index, const char* content) {
-				if (index == -1) {
-					ss += "<eop>\n";
-				} else {
-					ss += std::string(content);
-				}
-				});
-            waiting = false;
-
-            history += ("答：" + ret + "\n");
+            session->history += ("[Round " + std::to_string(session->round++) + "]\n问：" + input);
+            auto prompt = session->round > 1 ? session->history : input;
+            auto inputs = model.weight.tokenizer.Encode(prompt);
+            std::vector<int> tokens;
+            for (int i = 0; i < inputs.Count(0); i++) {
+                tokens.push_back(((float *) inputs.cpuData)[i]);
+            }
+            int handleId = model.LaunchResponseTokens(tokens);
+            std::vector<float> results;
+            while (true) {
+                auto result = model.FetchResponseTokens(handleId);
+                if (result.first == false) {
+                    break;
+                } else {
+                    results.clear();
+                    results.push_back(result.second[0]);
+                    session->output += model.weight.tokenizer.Decode(fastllm::Data (fastllm::DataType::FLOAT32, {(int)results.size()}, results));
+                }
+                if (session->status == 2) {
+                    break;
+                }
+            }
+            session->history += ("答：" + session->output + "\n");
+            session->output += "<eop>\n";
+            session->status = 2;
         }
     };
 
     svr.Post("/chat", [&](const httplib::Request &req, httplib::Response &res) {
-        if (req.body == last_request) {
-            res.set_content(ss, "text/plain");
-            return;
+        const std::string uuid = req.get_header_value("uuid");
+        locker.lock();
+        if (sessions.find(uuid) == sessions.end()) {
+            sessions[uuid] = new ChatSession();
         }
-        if (waiting) {
-            res.set_content(ss, "text/plain");
+        auto *session = sessions[uuid];
+        locker.unlock();
+
+        if (session->status != 0) {
+            res.set_content(session->output, "text/plain");
+            if (session->status == 2) {
+                session->status = 0;
+            }
         } else {
-            ss = "";
-            last_request = req.body;
-            std::thread chat_thread(chat, last_request);
+            session->output = "";
+            session->status = 1;
+            std::thread chat_thread(chat, session, req.body);
             chat_thread.detach();
         }
     });
 
-    svr.set_mount_point("/", "web");
+    svr.set_mount_point("/", "../example/webui/web");
     std::cout << ">>> please open http://127.0.0.1:8081\n";
     svr.listen("0.0.0.0", 8081);
 
