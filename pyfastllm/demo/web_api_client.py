f315bea4c56b3463c3d0fd5f360411bec1bbe93d
kiranosora <kiranosora@gmail.com>
2023-07-10 23:41:32 +0800

    fix py_api, add GenerationConfig && fix pyfastllm/build_libs.py

--- pyfastllm/demo/web_api_client.py
+++ pyfastllm/demo/web_api_client.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == '__main__':
     url = 'http://127.0.0.1:8000/api/chat_stream'
-    prompt='请用emoji写一首长诗赞美世界'
+    prompt='请用emoji写一首诗赞美世界'
     json_obj = {"uid":0, "token":"xxxxxxxxxxxxxxxxx","history": "", "prompt": prompt , "max_length": None, "top_p": None,"temperature": None}
     response = requests.post(url, json=json_obj,  stream = True)
     try:
@@ -14,7 +14,7 @@ if __name__ == '__main__':
         print("response:")
         for chunk in response.iter_content(chunk_size=1024*1024):
             msg = chunk.decode(errors='replace')
-            if len(msg) > len(pre_msg) and msg[len(pre_msg):][-1] == '\n':
+            if len(msg) > len(pre_msg) and msg[-1] == '\n':
                 content = msg[len(pre_msg):]
                 pre_msg = msg
             else:
