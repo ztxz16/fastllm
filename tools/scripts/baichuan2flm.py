cc67f8841e84f51a5a790a776901e7a31839a9ea
huangyuyang <410644548@qq.com>
2023-07-12 13:27:53 +0800

    修改百川的转换文件

---
+++ tools/scripts/baichuan2flm.py
@@ -0,0 +1,18 @@
+import sys
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers.generation.utils import GenerationConfig
+from fastllm_pytools import torch2flm
+
+if __name__ == "__main__":
+    exportPath = sys.argv[1] if (sys.argv[1] is not None) else "baichuan-13b-fp32.flm";
+
+    #modelpath = "baichuan-inc/baichuan-13B-"
+    modelpath = "/root/Baichuan-13B-Chat"
+    tokenizer = AutoTokenizer.from_pretrained(modelpath, trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(modelpath, device_map="auto", trust_remote_code=True)
+    try:
+        model.generation_config = GenerationConfig.from_pretrained(path);
+    except:
+        pass;
+    torch2flm.tofile(exportPath, model, tokenizer);
