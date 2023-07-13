4717baa40d20c07a808ca982e9b35b1b173af82c
huangyuyang <410644548@qq.com>
2023-07-13 16:38:20 +0800

    支持baichuan-13b模型直接转换

--- tools/scripts/baichuan2flm.py
+++ tools/scripts/baichuan2flm.py
@@ -7,12 +7,11 @@ from fastllm_pytools import torch2flm
 if __name__ == "__main__":
     exportPath = sys.argv[1] if (sys.argv[1] is not None) else "baichuan-13b-fp32.flm";
 
-    #modelpath = "baichuan-inc/baichuan-13B-"
-    modelpath = "/root/Baichuan-13B-Chat"
+    modelpath = "baichuan-inc/baichuan-13B-Chat"
     tokenizer = AutoTokenizer.from_pretrained(modelpath, trust_remote_code=True)
     model = AutoModelForCausalLM.from_pretrained(modelpath, device_map="auto", trust_remote_code=True)
     try:
-        model.generation_config = GenerationConfig.from_pretrained(path);
+        model.generation_config = GenerationConfig.from_pretrained(modelpath);
     except:
         pass;
     torch2flm.tofile(exportPath, model, tokenizer);
