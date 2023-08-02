254b7470d50ca2d629e110f71e8be178f0fc1523
ztxz16 <410644548@qq.com>
2023-08-02 14:55:55 +0800

    Merge pull request #186 from denghongcai/feat-optimize-web-demo
    feat: 🎸 optimize web demo

--- tools/scripts/web_demo.py
+++ tools/scripts/web_demo.py
@@ -10,30 +10,28 @@ st.set_page_config(
 
 @st.cache_resource
 def get_model():
-    model = llm.model(sys.argv[1]);
-    return model;
+    model = llm.model(sys.argv[1])
+    return model
 
-def predict(input, history = None):
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for i, (prompt, response) in enumerate(st.session_state.messages):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    with st.chat_message("assistant"):
+        st.markdown(response)
+
+if prompt := st.chat_input("请开始对话"):
     model = get_model()
-    if history is None:
-        history = []
-    with container:
-        if len(history) > 0:
-            for i, (query, response) in enumerate(history):
-                message(query, avatar_style = "big-smile", key = str(i) + "_user")
-                message(response, avatar_style = "bottts", key = str(i))
-        message(input, avatar_style = "big-smile", key = str(len(history)) + "_user")
-        st.write("AI正在回复:")
-        with st.empty():
-            for response in model.stream_response(input, history, one_by_one = False):
-                st.write(response)
-            history.append((input, response));
-    return history
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-container = st.container()
-prompt_text = st.text_area(label="用户命令输入", height = 100, placeholder = "请在这儿输入您的命令")
-if 'state' not in st.session_state:
-    st.session_state['state'] = []
-if st.button("发送", key = "predict"):
-    with st.spinner("AI正在思考，请稍等........"):
-        st.session_state["state"] = predict(prompt_text, st.session_state["state"])
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for chunk in model.stream_response(prompt, st.session_state.messages, one_by_one = True):
+            full_response += chunk
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append((prompt, full_response))
