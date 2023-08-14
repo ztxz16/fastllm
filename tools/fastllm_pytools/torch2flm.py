4ba5fd53fb11f1ffc6abc391472b1de9e06195ab
huangyuyang <huang.yuyang@think-force.com>
2023-08-14 13:51:50 +0800

    fix

--- tools/fastllm_pytools/torch2flm.py
+++ tools/fastllm_pytools/torch2flm.py
@@ -94,6 +94,9 @@ def tofile(exportPath,
         modelInfo["user_role"] = ("<FLM_FIX_TOKEN_" + str(model.generation_config.user_token_id) + "> ") if hasattr(model.generation_config, "user_token_id") else "";
         modelInfo["bot_role"] = ("<FLM_FIX_TOKEN_" + str(model.generation_config.assistant_token_id) + ">") if hasattr(model.generation_config, "assistant_token_id") else "";
         modelInfo["history_sep"] = ""
+    if modelInfo["model_type"] == "qwen":
+        modelInfo["im_end_id"] = tokenizer.im_end_id
+        modelInfo["im_start_id"] = tokenizer.im_start_id
 
     modelInfo["tokenizer_use_score"] = "1" # 分词带分数
 
