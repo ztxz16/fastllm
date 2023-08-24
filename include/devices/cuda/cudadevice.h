666e709c69bb7b3bb5a6c8ff31d8faf3f432279c
huangyuyang <410644548@qq.com>
2023-08-24 14:33:51 +0800

    attention先提交以下（暂未使用）

--- include/devices/cuda/cudadevice.h
+++ include/devices/cuda/cudadevice.h
@@ -19,6 +19,11 @@ namespace fastllm {
         bool CopyDataFromCPU(void *dst, void *src, size_t size);
     };
 
+    class CudaAttention : BaseOperator {
+        void Reshape(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
+        void Run(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
+    };
+
     class CudaLayerNormOp : BaseOperator {
         bool CanRun(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
         void Run(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
