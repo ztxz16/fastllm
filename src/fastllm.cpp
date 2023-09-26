021802431156f5a8178e2ff9addabcec8295b128
fluxlinkage <fluxlinkage@sina.com>
2023-09-26 09:13:49 +0800

    改进分词。

--- src/fastllm.cpp
+++ src/fastllm.cpp
@@ -920,9 +920,6 @@ namespace fastllm {
             std::string blank = "";
             blank += 226, blank += 150, blank += 129;
             std::string s = blank;
-            if (15 < ori.size() && ori.substr(0, 15) == "<FLM_FIX_TOKEN_") {
-                s = "";
-            }
             for (int i = 0; i < ori.size(); i++) {
                 if (ori[i] == ' ') {
                     if (i != 0 && ori[i - 1] != ' ') {
@@ -932,106 +929,103 @@ namespace fastllm {
                     s += ori[i];
                 }
             }
-
-            std::vector<Symbol> symbols;
-            for (int i = 0; i < s.size(); i++) {
-                if (i + 3 < s.size() && s[i] == '<' && s[i + 1] == 'F' && s[i + 2] == 'L' && s[i + 3] == 'M') {
-                    if (i + 15 < s.size() && s.substr(i, 15) == "<FLM_FIX_TOKEN_") {
-                        i += 15;
-                        int now = 0;
-                        while (s[i] >= '0' && s[i] <= '9') {
-                            now = now * 10 + s[i] - '0';
-                            i++;
-                        }
-                        symbols.push_back(Symbol(nullptr, (char *) s.data(), i, 0, (int) symbols.size() - 1,
-                                                 (int) symbols.size() + 1, now));
-                        continue;
+            std::vector<float> v;
+            int findPos=0;
+            while(findPos<s.length()){
+                int nextSpecialToken=-1;
+                int nextSpecialTokenPos=-1;
+                int nextSpecialTokenLen=-1;
+                for(auto p:specialTokens){
+                    int ind=s.find(p.first,findPos);
+                    if(ind>=0&&(nextSpecialTokenPos<0||ind<nextSpecialTokenPos)){
+                        nextSpecialTokenPos=ind;
+                        nextSpecialToken=p.second;
+                        nextSpecialTokenLen=p.first.length();
                     }
                 }
-
-                int tokenId = -999999, pos = i - 1;
-                TrieNode *now = this->root;
-                for(const auto &special:specialTokens){
-                    const std::string &tokenTxt=special.first;
-                    int sz=tokenTxt.size();
-                    if(i+sz<=s.size()&&s.substr(i,sz)==tokenTxt){
-                        tokenId=special.second;
-                        pos=i+sz-1;
-                        for(int k=0;k<sz;k++){
-                            now=now->next[s[i+k]];
-                        }
-                        break;
-                    }
+                std::string subStr;
+                if(nextSpecialTokenPos<0){
+                    subStr=s.substr(findPos);
+                    findPos=s.length();
+                }else{
+                    subStr=s.substr(findPos,nextSpecialTokenPos-findPos);
+                    findPos=nextSpecialTokenPos+nextSpecialTokenLen;
                 }
-                if(tokenId<0){
-                    for (int j = i; j < s.size(); j++) {
-                        if (now->next.find(s[j]) != now->next.end()) {
-                            now = now->next[s[j]];
-                            if (now->tokenId != -999999) {
-                                tokenId = now->tokenId;
-                                pos = j;
+                if(subStr.length()>0){
+                    std::vector<Symbol> symbols;
+                    for (int i = 0; i < subStr.size(); i++) {
+                        int tokenId = -999999, pos = i - 1;
+                        TrieNode *now = this->root;
+                        for (int j = i; j < subStr.size(); j++) {
+                            if (now->next.find(subStr[j]) != now->next.end()) {
+                                now = now->next[subStr[j]];
+                                if (now->tokenId != -999999) {
+                                    tokenId = now->tokenId;
+                                    pos = j;
+                                    break;
+                                }
+                            } else {
                                 break;
                             }
+                        }
+                        if (pos >= i) {
+                            symbols.push_back(Symbol(now, (char *) subStr.data(), i, pos - i + 1, (int) symbols.size() - 1,
+                                                     (int) symbols.size() + 1, -999999));
+                            i = pos;
                         } else {
-                            break;
+                            symbols.push_back(Symbol(nullptr, (char *) subStr.data(), i, 0, (int) symbols.size() - 1,
+                                                     (int) symbols.size() + 1, -999999));
+                        }
+                    }
+                    symbols.back().next = -1;
+
+                    std::priority_queue<SymbolPairs> workQueue;
+                    for (int i = 1; i < symbols.size(); i++) {
+                        TryMergePairs(symbols, i - 1, i, workQueue);
+                    }
+
+                    while (!workQueue.empty()) {
+                        auto top = workQueue.top();
+                        workQueue.pop();
+                        if (symbols[top.l].len == 0 || symbols[top.r].len == 0 ||
+                                symbols[top.l].len + symbols[top.r].len != top.size) {
+                            continue;
+                        }
+
+                        for (int i = symbols[top.r].pos; i < symbols[top.r].pos + symbols[top.r].len; i++) {
+                            symbols[top.l].node = symbols[top.l].node->next[symbols[top.r].s[i]];
+                        }
+                        symbols[top.l].len += symbols[top.r].len;
+                        symbols[top.r].len = 0;
+                        symbols[top.l].next = symbols[top.r].next;
+                        if (symbols[top.r].next >= 0) {
+                            symbols[symbols[top.r].next].prev = top.l;
+                        }
+
+                        TryMergePairs(symbols, symbols[top.l].prev, top.l, workQueue);
+                        TryMergePairs(symbols, top.l, symbols[top.l].next, workQueue);
+                    }
+                    for (int i = 0; i < symbols.size(); i++) {
+                        if (symbols[i].len > 0) {
+                            v.push_back(symbols[i].node->tokenId);
+                        } else if (symbols[i].node == nullptr) {
+                            if (symbols[i].fixId != -999999) {
+                                v.push_back(symbols[i].fixId);
+                            } else {
+                                // 未识别的字符
+                                uint8_t c = (uint8_t) (symbols[i].s[symbols[i].pos]);
+                                std::string now = "<0x00>";
+                                now[3] = (c / 16 > 9 ? ('A' + c / 16 - 10) : ('0' + c / 16));
+                                now[4] = (c % 16 > 9 ? ('A' + c % 16 - 10) : ('0' + c % 16));
+                                if (stringToTokenDict.find(now) != stringToTokenDict.end()) {
+                                    v.push_back(stringToTokenDict[now]);
+                                }
+                            }
                         }
                     }
                 }
-                if (pos >= i) {
-                    symbols.push_back(Symbol(now, (char *) s.data(), i, pos - i + 1, (int) symbols.size() - 1,
-                                             (int) symbols.size() + 1, -999999));
-                    i = pos;
-                } else {
-                    symbols.push_back(Symbol(nullptr, (char *) s.data(), i, 0, (int) symbols.size() - 1,
-                                             (int) symbols.size() + 1, -999999));
-                }
-            }
-            symbols.back().next = -1;
-
-            std::priority_queue<SymbolPairs> workQueue;
-            for (int i = 1; i < symbols.size(); i++) {
-                TryMergePairs(symbols, i - 1, i, workQueue);
-            }
-
-            while (!workQueue.empty()) {
-                auto top = workQueue.top();
-                workQueue.pop();
-                if (symbols[top.l].len == 0 || symbols[top.r].len == 0 ||
-                    symbols[top.l].len + symbols[top.r].len != top.size) {
-                    continue;
-                }
-
-                for (int i = symbols[top.r].pos; i < symbols[top.r].pos + symbols[top.r].len; i++) {
-                    symbols[top.l].node = symbols[top.l].node->next[symbols[top.r].s[i]];
-                }
-                symbols[top.l].len += symbols[top.r].len;
-                symbols[top.r].len = 0;
-                symbols[top.l].next = symbols[top.r].next;
-                if (symbols[top.r].next >= 0) {
-                    symbols[symbols[top.r].next].prev = top.l;
-                }
-
-                TryMergePairs(symbols, symbols[top.l].prev, top.l, workQueue);
-                TryMergePairs(symbols, top.l, symbols[top.l].next, workQueue);
-            }
-
-            std::vector<float> v;
-            for (int i = 0; i < symbols.size(); i++) {
-                if (symbols[i].len > 0) {
-                    v.push_back(symbols[i].node->tokenId);
-                } else if (symbols[i].node == nullptr) {
-                    if (symbols[i].fixId != -999999) {
-                        v.push_back(symbols[i].fixId);
-                    } else {
-                        // 未识别的字符
-                        uint8_t c = (uint8_t) (symbols[i].s[symbols[i].pos]);
-                        std::string now = "<0x00>";
-                        now[3] = (c / 16 > 9 ? ('A' + c / 16 - 10) : ('0' + c / 16));
-                        now[4] = (c % 16 > 9 ? ('A' + c % 16 - 10) : ('0' + c % 16));
-                        if (stringToTokenDict.find(now) != stringToTokenDict.end()) {
-                            v.push_back(stringToTokenDict[now]);
-                        }
-                    }
+                if(nextSpecialTokenPos>=0){
+                    v.push_back(nextSpecialToken);
                 }
             }
             return Data (DataType::FLOAT32, {1, (int)v.size()}, v);
