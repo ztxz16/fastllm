30a5a0d8707c4e9237e2ad6f077ca7ff3a1faea2
wildkid1024 <wildkid1024@outlook.com>
2023-10-13 19:35:55 +0800

    修复wheel构建

--- pyfastllm/examples/cli_simple.py
+++ pyfastllm/examples/cli_simple.py
@@ -3,20 +3,19 @@ import sys
 import platform
 import logging
 import argparse
-
-sys.path.append('./build-py')
-import pyfastllm as fastllm # 或fastllm
+import fastllm
 
 logging.info(f"python gcc version:{platform.python_compiler()}")
 
 def args_parser():
-    parser = argparse.ArgumentParser(description='pyfastllm')
+    parser = argparse.ArgumentParser(description='fastllm')
     parser.add_argument('-m', '--model', type=int, required=False, default=0, help='模型类型，默认为0, 可以设置为0(chatglm),1(moss),2(vicuna),3(baichuan)')
     parser.add_argument('-p', '--path', type=str, required=True, default='', help='模型文件的路径')
     parser.add_argument('-t', '--threads', type=int, default=4,  help='使用的线程数量')
     parser.add_argument('-l', '--low', action='store_true', help='使用低内存模式')
     args = parser.parse_args()
     return args
+
 
 def response(model, prompt_input:str, stream_output:bool=False):
     gmask_token_id = 130001
@@ -47,8 +46,7 @@ def response(model, prompt_input:str, stream_output:bool=False):
 
     return ret_str
 
-
-def main(args):
+def run_with_response(args):
     model_path = args.path
     OLD_API = False
     if OLD_API:
@@ -70,6 +68,37 @@ def main(args):
             print(f"\r{model.model_type}: {output}", end='', flush=True)
         print()
 
+
+def run_with_callback(args):
+    model_path = args.path
+    OLD_API = False
+    LLM_TYPE = ""
+    if OLD_API:
+        model = fastllm.ChatGLMModel()
+        model.load_weights(model_path)
+        model.warmup()
+    else:
+        LLM_TYPE = fastllm.get_llm_type(model_path)
+        model = fastllm.create_llm(model_path)
+    
+    def print_back(idx:int, content: bytearray):
+        content = content.decode(encoding="utf-8", errors="replace")
+        if idx >= 0:
+            print(f"\r{LLM_TYPE}:{content}", end='', flush=True)
+        elif idx == -1:
+            print()
+        sys.stdout.flush()
+        
+    prompt = ""
+    while prompt != "stop":
+        prompt = input("User: ")
+        config = fastllm.GenerationConfig()
+        model.response(model.make_input("", 0, prompt), print_back, config)
+        print()
+        sys.stdout.flush()
+
+
 if __name__ == "__main__":
     args = args_parser()
-    main(args)
+    # run_with_callback(args)
+    run_with_response(args)
