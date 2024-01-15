bed1ac49396a351fe0e507c1828bd79982a7d635
ztxz16 <410644548@qq.com>
2024-01-15 17:00:42 +0800

    Merge pull request #400 from TylunasLi/bug_fix_att_opt
    兼容chatglm3-6b-32k，修复model.save()保存的模型tokenizer未对齐问题。

--- src/fastllm.cpp
+++ src/fastllm.cpp
@@ -814,6 +814,7 @@ namespace fastllm {
         now->tokenId = tokenId;
         now->score = score;
         tokenToStringDict[tokenId] = s;
+        tokenToScoreDict[tokenId] = score;
         stringToTokenDict[s] = tokenId;
     }
 
@@ -859,9 +860,10 @@ namespace fastllm {
             }
             for (int i = 0; i < ori.size(); i++) {
                 if (ori[i] == ' ') {
-                    if (i != 0 && ori[i - 1] != ' ') {
-                        s += blank;
-                    }
+                    // if (i != 0 && ori[i - 1] != ' ') {
+                        // s += blank;
+                    // }
+                    s += blank;
                 } else {
                     s += ori[i];
                 }
