f64d250af003b6805b3fe0e7e4323d2bf26c07d4
cgli <cgli@iyunwen.com>
2024-02-11 18:08:48 +0800

    基本对齐huggingface tokenizers的BPE Tokenizer

--- tools/scripts/alpaca2flm.py
+++ tools/scripts/alpaca2flm.py
@@ -1,11 +1,13 @@
 import sys
-from transformers import LlamaTokenizer, LlamaForCausalLM
+import torch
+from transformers import AutoTokenizer, LlamaForCausalLM
 from fastllm_pytools import torch2flm
 
 if __name__ == "__main__":
     model_name = sys.argv[3] if len(sys.argv) >= 4 else 'minlik/chinese-alpaca-33b-merged'
-    tokenizer = LlamaTokenizer.from_pretrained(model_name)
-    model = LlamaForCausalLM.from_pretrained(model_name).float()
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    # `torch_dtype=torch.float16` is set by default, if it will not cause an OOM Error, you can load model in float32.
+    model = LlamaForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16)
     conf = model.config.__dict__
     conf["model_type"] = "llama"
     dtype = sys.argv[2] if len(sys.argv) >= 3 else "float16"
