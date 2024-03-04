7272d016030981215c6765a17cfab436dce78d3b
cgli <cgli@iyunwen.com>
2024-03-04 22:11:24 +0800

    MiniCpmModel类作为LlamaModel类的子类，优化脚本

--- include/models/minicpm.h
+++ include/models/minicpm.h
@@ -6,12 +6,13 @@
 #define FASTLLM_MINICPM_H
 
 #include "basellm.h"
+#include "llama.h"
 #include "cmath"
 
 #include <iostream>
 
 namespace fastllm {
-    class MiniCpmModel: public basellm {
+    class MiniCpmModel: public LlamaModel {
     public:
         MiniCpmModel(); // 构造函数
 
@@ -47,26 +48,6 @@ namespace fastllm {
                 const std::vector <GenerationConfig> &generationConfigs,
                 const LastTokensManager &lastTokens = LastTokensManager(),
                 std::vector <std::vector <float>*> *logits = nullptr);
-
-        virtual std::string Response(const std::string& input,
-                                     RuntimeResult retCb,
-                                     const GenerationConfig &generationConfig = GenerationConfig()); // 根据给出的内容回复
-
-        virtual void ResponseBatch(const std::vector <std::string> &inputs,
-                                   std::vector <std::string> &outputs,
-                                   RuntimeResultBatch retCb,
-                                   const GenerationConfig &generationConfig = GenerationConfig());
-
-        virtual int LaunchResponseTokens(const std::vector <int> &inputTokens,
-                                         const GenerationConfig &generationConfig = GenerationConfig()); // 启动一个response任务，返回分配的handleId
-
-        virtual int FetchResponseTokens(int handelId); // 获取指定handle的输出, -1代表输出结束了
-
-        virtual void WarmUp(); // 预热
-
-        virtual std::string MakeInput(const std::string &history, int round, const std::string &input); // 根据历史信息和当前输入生成prompt
-
-        virtual std::string MakeHistory(const std::string &history, int round, const std::string &input, const std::string &output); // 根据当前回复更新history
 
     private:
         float embed_scale = 1.f;
