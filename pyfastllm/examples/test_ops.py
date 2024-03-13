e161065f53c9d60f66a2d21619ba3014ac9852f6
wildkid1024 <wildkid1024@outlook.com>
2024-03-13 18:06:07 +0800

    python端完整支持chatglm2

--- pyfastllm/examples/test_ops.py
+++ pyfastllm/examples/test_ops.py
@@ -6,15 +6,17 @@ import fastllm
 import pyfastllm
 import gc
 
-# import np_ops
-# import ops as flm_ops
+import np_ops
+import ops as flm_ops
 
-from fastllm import ops as flm_ops
-from fastllm import np_ops
+# from fastllm import ops as flm_ops
+# from fastllm import np_ops
 
 np.random.seed(42)
 
 def diff(dataA, dataB):
+    # print(dataA)
+    # print(dataB)
     mae = np.max(np.abs(dataA - dataB))
     print('max abs err is ', mae)
     return mae
@@ -23,6 +25,7 @@ def to_tensor(data):
     return pyfastllm.from_numpy(data)
 
 def to_numpy(data):
+    # return data.numpy()
     return np.array(data, copy=False, order='C')
 
 def test_rms_norm(inputs=None, weights=None, eps=1e-6):
@@ -38,19 +41,19 @@ def test_rms_norm(inputs=None, weights=None, eps=1e-6):
 
 def test_swiglu(inputs=None):
     if not inputs:
-        inputs = np.array([1, 5]).reshape([1, 2])
+        inputs = np.random.random(size=[1, 256])
     
     np_out = np_ops.swiglu(inputs)
-    out = flm_ops.activation(input=to_tensor(inputs), activate_type="swiglu")
-    mae = diff(np_out, out)
+    out = flm_ops.activation(inputs=to_tensor(inputs), activate_type="swiglu")
+    mae = diff(np_out, to_numpy(out))
     assert mae <= 1e-6
     return out
     
 def test_attention(q=None, k=None, v=None, mask=None, group=1, scale=1.0):
     if q is None:
-        q = np.random.random(size=[1, 12, 4096])
-        k = np.random.random(size=[1, 12, 4096])
-        v = np.random.random(size=[1, 12, 4096])
+        q = np.random.random(size=[12, 1, 4096])
+        k = np.random.random(size=[12, 1, 4096])
+        v = np.random.random(size=[12, 1, 4096])
         scale = 1 / np.sqrt(q.shape[-1])
 
     np_out = np_ops.attention(q, k, v, scale=scale)
@@ -68,18 +71,20 @@ def test_linear(inputs=None,
            bias=None):
     
     if not inputs:
-        inputs = np.random.random(size=[1, 2, 4096])
+        inputs = np.random.random(size=[1, 12, 4096])
+        weights = np.random.random(size=[256, 4096])
         
     np_out = np_ops.linear(inputs=inputs, weights=weights, bias=None)
 
     if not bias:
         bias = fastllm.Tensor()
 
-    output = flm_ops.linear(to_tensor(inputs), weights, bias)
+    output = flm_ops.linear(to_tensor(inputs), to_tensor(weights), bias)
     mae = diff(np_out, to_numpy(output))
 
-    assert mae <= 1e-6
+    assert mae <= 1e-3
     return output
+
 
 if __name__ == "__main__":    
     test_rms_norm()
--- pyfastllm/fastllm/functions/numpy_ops.py
+++ pyfastllm/fastllm/functions/numpy_ops.py
@@ -18,7 +18,7 @@ def layer_norm(inputs, gamma, beta, axis=-1):
     output = (inputs - mean) / var * gamma + beta
     return output
 
-@jit(nopython=True)
+# @jit
 def softmax(inputs, axis=None):
     maxv = inputs.max(axis, keepdims=True)
     exp_v = np.exp(inputs - maxv)
@@ -29,13 +29,38 @@ def softmax(inputs, axis=None):
 def silu(inputs, ):
     return inputs / (1 + np.exp(-inputs))
 
-@jit(nopython=True)
+@jit
+def swiglu(inputs, ):
+    dim = inputs.shape[1] // 2 
+    for batch in range(inputs.shape[0]):
+        return inputs[batch, :dim] / (1 + np.exp(-inputs[batch, :dim])) * inputs[batch, dim:]
+
+# @jit
 def linear(inputs, weights, bias):
-    output = np.matmul(inputs, weights.T) + bias
+    if len(inputs.shape) == 2:
+        inputs = inputs[None, :]
+        weights = weights[None, :]
+    
+    output = np.zeros(shape=[inputs.shape[0], inputs.shape[1], weights.shape[0]])
+    for batch in range(inputs.shape[0]):
+        output[batch] = np.matmul(inputs[batch], weights.T)
+
+        if bias:
+            output[batch] += bias[batch]
+
     return output
 
-@jit(nopython=True)    
-def np_self_attention(q, k, v, mask=None, group=None, scale=None):
-    qk = softmax(q @ k.T * scale, axis=-1)
-    attn = qk @ v
+# @jit
+def attention(q, k, v, mask=None, group=None, scale=None):
+    print("shape:", q.shape)
+    if len(q.shape) == 2:
+        q = q[None, :]
+        k = k[None, :]
+        v = v[None, :]
+        # mask = mask[None, :]
+    
+    attn = np.zeros_like(q)
+    for batch in range(q.shape[0]):
+        qk = softmax(q[batch] @ k[batch].T * scale, axis=-1)
+        attn[batch, :, :] = qk @ v[batch]
     return attn
