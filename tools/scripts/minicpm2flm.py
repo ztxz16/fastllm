1ab1a04cce2ad6afaa5279655def5c3d18441771
ztxz16 <410644548@qq.com>
2024-03-20 11:51:34 +0800

    Merge pull request #438 from TylunasLi/minicpm
    修复Win32Demo CPU构建错误

--- tools/scripts/minicpm2flm.py
+++ tools/scripts/minicpm2flm.py
@@ -5,7 +5,7 @@ from fastllm_pytools import torch2flm
 
 if __name__ == "__main__":
     modelNameOrPath = sys.argv[3] if len(sys.argv) >= 4 else "openbmb/MiniCPM-2B-dpo-fp16"
-    tokenizer = AutoTokenizer.from_pretrained(modelNameOrPath, trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(modelNameOrPath, use_fast=False, trust_remote_code=True)
     # `torch_dtype=torch.float16` is set by default, if it will not cause an OOM Error, you can load model in float32.
     model = AutoModelForCausalLM.from_pretrained(modelNameOrPath, trust_remote_code=True, torch_dtype=torch.float16)
     model = model.eval()
@@ -14,6 +14,6 @@ if __name__ == "__main__":
 
     dtype = sys.argv[2] if len(sys.argv) >= 3 else "float16"
     exportPath = sys.argv[1] if len(sys.argv) >= 2 else "minicpm-2b-" + dtype + ".flm"
-    torch2flm.tofile(exportPath, model, tokenizer, pre_prompt = "", 
+    torch2flm.tofile(exportPath, model, tokenizer, pre_prompt = "<s>", 
                      user_role = "<用户>", bot_role = "<AI>", 
                      history_sep = "", dtype = dtype)
