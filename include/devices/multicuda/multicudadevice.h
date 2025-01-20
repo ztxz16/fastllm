0755a3c84a3b2866374ddc54c05e6d4a37a86f37
黄宇扬 <huang.yuyang@think-force.com>
2025-01-20 11:36:34 +0800

    试验性加入多卡MLP合并

--- include/devices/multicuda/multicudadevice.h
+++ include/devices/multicuda/multicudadevice.h
@@ -9,17 +9,36 @@
 
 namespace fastllm {
     class MultiCudaDevice : BaseDevice {
+        
+    private:
+        CudaDevice *cudaDevice;
+
     public:
-        MultiCudaDevice ();
+        MultiCudaDevice (CudaDevice *cudaDevice);
 
         bool Malloc (void **ret, size_t size); // 分配尺寸为size的空间
         bool Free(void *ret); // 释放ret
 
         bool CopyDataToCPU(void *dst, void *src, size_t size);
         bool CopyDataFromCPU(void *dst, void *src, size_t size);
+
+        // 是否可以运行某一个算子
+        bool CanRun(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
+
+        // 对某一个算子进行形状推理
+        void Reshape(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
+
+        // 对某一个算子进行推理
+        void Run(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
     };
 
     class MultiCudaLinearOp : CudaLinearOp {
+        bool CanRun(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
+        void Run(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
+    };
+
+    class MultiCudaMLPOp : CudaLinearOp {
+        void Reshape(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
         void Run(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
     };
 }
