71396101cfa86941623d813b65dfb084ca7860be
黄宇扬 <410644548@qq.com>
2025-01-20 11:36:34 +0800

    试验性加入多卡MLP合并

--- include/devices/multicuda/fastllm-multicuda.cuh
+++ include/devices/multicuda/fastllm-multicuda.cuh
@@ -15,6 +15,8 @@ void FastllmMultiCudaSetDevice(std::vector <int> ids);
 bool FastllmMultiCudaHalfMatMul(const fastllm::Data &input, fastllm::Data &weight, const fastllm::Data &bias, fastllm::Data &output, int n, int m, int k);
 bool FastllmMultiCudaMatMul(const fastllm::Data &input, fastllm::Data &weight, const fastllm::Data &bias, fastllm::Data &output, int n, int m, int k);
 
+bool FastllmMultiCudaMLP(const fastllm::Data &input, fastllm::Data &weight0, fastllm::Data &weight1, fastllm::Data &output);
+
 #ifdef  __cplusplus
 }
 #endif
