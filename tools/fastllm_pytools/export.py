0569b14e216badf2094f30a6bc6881d35e286b5a
黄宇扬 <410644548@qq.com>
2025-02-21 11:08:57 +0800

    增加导出模型为safetensors模式的功能

---
+++ tools/fastllm_pytools/export.py
@@ -0,0 +1,15 @@
+import argparse
+from ftllm import llm
+from .util import make_normal_parser
+from .util import make_normal_llm_model
+import readline
+
+def args_parser():
+    parser = make_normal_parser('fastllm_export')
+    parser.add_argument('-o', '--output', type = str, required = True, help = '导出路径')
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = args_parser()
+    llm.export_llm_model_fromhf(path = args.path, dtype = args.dtype, lora = args.lora, output = args.output)
