3764677ea108d466ceffec139608df9d63afde62
黄宇扬 <huang.yuyang@think-force.com>
2025-03-31 03:12:38 +0800

    对话时读取模型的generation_config.json（若有）

--- tools/fastllm_pytools/chat.py
+++ tools/fastllm_pytools/chat.py
@@ -10,6 +10,23 @@ def args_parser():
 def fastllm_chat(args):
     from .util import make_normal_llm_model
     model = make_normal_llm_model(args)
+
+    generation_config = {
+        'repetition_penalty': 1.0,
+        'top_p': 0.8,
+        'top_k': 1,
+        'temperature': 1.0
+    }
+    import os
+    import json
+    if (os.path.exists(os.path.join(args.path, "generation_config.json"))):
+        with open(os.path.join(args.path, "generation_config.json"), "r", encoding="utf-8") as file:
+            config = json.load(file)
+            if ('do_sample' in config and config['do_sample']):
+                for it in ["repetition_penalty", "top_p", "top_k", "temperature"]:
+                    if (it in config):
+                        generation_config[it] = config[it];
+
     hint = "输入内容开始对话\n'clear'清空记录\n'stop'终止程序."
     history = []
 
@@ -24,7 +41,11 @@ def fastllm_chat(args):
             continue
         print("AI:", end = "");
         curResponse = "";
-        for response in model.stream_response(query, history = history):
+        for response in model.stream_response(query, history = history, 
+                                              repeat_penalty = generation_config["repetition_penalty"],
+                                              top_p = generation_config["top_p"],
+                                              top_k = generation_config["top_k"],
+                                              temperature = generation_config["temperature"]):
             curResponse += response;
             print(response, flush = True, end = "")
         history.append((query, curResponse))
