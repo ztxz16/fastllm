7fe1496f0f8056cac253987b3f4c7c3acccfb0f2
黄宇扬 <410644548@qq.com>
2025-04-11 17:41:52 +0800

    增加配置文件

--- tools/fastllm_pytools/cli.py
+++ tools/fastllm_pytools/cli.py
@@ -3,6 +3,40 @@ from .util import make_normal_parser
 import readline
 import os
 
+def save_defaults_to_json(parser, filename):
+    # 获取所有参数的默认值
+    defaults = {}
+
+    # 添加位置参数的默认值
+    for action in parser._actions:
+        if not action.option_strings and action.dest != 'help':
+            defaults[action.dest] = action.default if action.default is not None else ""
+    if defaults["model"] == '':
+        defaults["model"] = "Qwen/Qwen2-0.5B-Instruct"
+    for action in parser._actions:
+        # 跳过位置参数（没有option_strings的）
+        if not action.option_strings:
+            continue
+            
+        # 获取参数名（选择最长的选项名，去掉前面的--或-）
+        name = max(action.option_strings, key=len).lstrip('-')
+        if (name == 'low' or name == 'path'):
+            continue
+        if (name == 'device'):
+            action.default = ""
+        # 处理store_true类型的参数
+        if action.const is True:
+            defaults[name] = False  # store_true参数的默认值是False
+        else:
+            defaults[name] = action.default
+    
+    # 将字典转换为JSON并保存到文件
+    import json
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(defaults, f, indent = 4, ensure_ascii = False)
+    
+    print("Create config to -> \"" + filename + "\"")
+
 def args_parser():
     parser = argparse.ArgumentParser(description = "fastllm")
     subparsers = parser.add_subparsers(dest='command', help='子命令')
@@ -32,12 +66,26 @@ def args_parser():
     server_parser_ = subparsers.add_parser('serve', parents = [server_parser], help = 'api模式')
     serve_parser_ = subparsers.add_parser('server', parents = [server_parser], help = 'api模式')
 
+    config_parser_ = subparsers.add_parser('config', help = '创建配置文件')
+    config_parser_.add_argument('file', nargs='?', help = '配置文件的路径')
+
     return parser
 
 def main():
     args = args_parser().parse_args()
     # 根据不同的子命令执行不同的操作
-    if args.command in ('chat', 'run'):
+    if args.command == 'config':
+        file = args.file
+        if not(file) or file == '':
+            file = "config.json"
+        if os.path.exists(file):
+            choice = input("File '" + file + "' exists，replace it? (Y/N): ").strip().upper()
+            if choice == 'Y':
+                pass
+            else:
+                return
+        save_defaults_to_json(make_normal_parser("fastllm", add_help = False), file)
+    elif args.command in ('chat', 'run'):
         from ftllm.chat import fastllm_chat
         fastllm_chat(args)
     elif args.command == "download":
