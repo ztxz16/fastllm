35f059423a6f3a211eaf8d5d33424e63eb1779fe
黄宇扬 <410644548@qq.com>
2025-04-15 21:04:11 +0800

    优化deep seek prefill速度

--- src/devices/cpu/linear.cpp
+++ src/devices/cpu/linear.cpp
@@ -111,6 +111,249 @@ namespace fastllm {
     }
 
     void MultiThreadLinearInt8Int4GroupOp::Run() {
+#ifdef __AVX2__
+        if (group == 1) {
+            int block = 0;
+            int realGroup = (m - 1) / groupCnt + 1;            
+            std::vector <float> tempValue, values;
+            tempValue.resize(n);
+            values.resize(n * k);
+            for (; block < n; block++) {
+                tempValue[block] = (inputSums[block] - izeros[block] * groupCnt) * iscales[block];
+            }
+
+/*
+            block = 0;
+            for (; block + 3 < n; block += 4) {
+                uint8_t *weightWalk = b;
+                uint8_t *inputStart = a + block * m;
+
+                for (int i = 0; i < k; i++) {
+                    uint8_t *a = weightWalk + (i * m) / 2;
+                    uint8_t *b = inputStart;
+
+                    __m512i acc0 = _mm512_setzero_si512();
+                    __m512i acc1 = _mm512_setzero_si512();
+                    __m512i acc2 = _mm512_setzero_si512();
+                    __m512i acc3 = _mm512_setzero_si512();
+
+                    const __m512i lowMask = _mm512_set1_epi8(0xf);
+                    const __m512i ones = _mm512_set1_epi16(1);
+                    int j = 0, ans = 0;
+                    for (; j + 63 < m; j += 64) {
+                        __m256i orix = _mm256_loadu_si256((const __m256i *) (a + j / 2));
+                        __m512i by0 = _mm512_loadu_si512((const __m512i *) (b + j));
+                        __m512i by1 = _mm512_loadu_si512((const __m512i *) (b + m * 1 + j));
+                        __m512i by2 = _mm512_loadu_si512((const __m512i *) (b + m * 2 + j));
+                        __m512i by3 = _mm512_loadu_si512((const __m512i *) (b + m * 3 + j));
+
+                        __m512i bytex = _mm512_inserti64x4(_mm512_castsi256_si512(orix), _mm256_srli_epi16(orix, 4), 1);
+                        __m512i bx = _mm512_and_si512(lowMask, bytex);
+
+                        acc0 = _mm512_dpbusd_epi32(acc0, by0, bx);
+                        acc1 = _mm512_dpbusd_epi32(acc1, by1, bx);
+                        acc2 = _mm512_dpbusd_epi32(acc2, by2, bx);
+                        acc3 = _mm512_dpbusd_epi32(acc3, by3, bx);
+                    }
+
+                    values[block * k + i] = _mm512_reduce_add_epi32(acc0);
+                    values[(block + 1) * k + i] = _mm512_reduce_add_epi32(acc1);
+                    values[(block + 2) * k + i] = _mm512_reduce_add_epi32(acc2);
+                    values[(block + 3) * k + i] = _mm512_reduce_add_epi32(acc3);
+                }
+            }
+
+            for (; block + 1 < n; block += 2) {
+                uint8_t *weightWalk = b;
+                uint8_t *inputStart = a + block * m;
+
+                for (int i = 0; i < k; i++) {
+                    uint8_t *a = weightWalk + (i * m) / 2;
+                    uint8_t *b = inputStart;
+
+                    __m512i acc0 = _mm512_setzero_si512();
+                    __m512i acc1 = _mm512_setzero_si512();
+
+                    const __m512i lowMask = _mm512_set1_epi8(0xf);
+                    const __m512i ones = _mm512_set1_epi16(1);
+                    int j = 0, ans = 0;
+                    for (; j + 63 < m; j += 64) {
+                        __m256i orix = _mm256_loadu_si256((const __m256i *) (a + j / 2));
+                        __m512i by0 = _mm512_loadu_si512((const __m512i *) (b + j));
+                        __m512i by1 = _mm512_loadu_si512((const __m512i *) (b + m * 1 + j));
+
+                        __m512i bytex = _mm512_inserti64x4(_mm512_castsi256_si512(orix), _mm256_srli_epi16(orix, 4), 1);
+                        __m512i bx = _mm512_and_si512(lowMask, bytex);
+
+                        acc0 = _mm512_dpbusd_epi32(acc0, by0, bx);
+                        acc1 = _mm512_dpbusd_epi32(acc1, by1, bx);
+                    }
+
+                    values[block * k + i] = _mm512_reduce_add_epi32(acc0);
+                    values[(block + 1) * k + i] = _mm512_reduce_add_epi32(acc1);
+                }
+            }
+
+            for (; block < n; block++) {
+                uint8_t *weightWalk = b;
+                uint8_t *inputStart = a + block * m;
+
+                for (int i = 0; i < k; i++) {
+                    uint8_t *a = weightWalk + (i * m) / 2;
+                    uint8_t *b = inputStart;
+
+                    __m512i acc = _mm512_setzero_si512();
+                    const __m512i lowMask = _mm512_set1_epi8(0xf);
+                    const __m512i ones = _mm512_set1_epi16(1);
+                    int j = 0, ans = 0;
+                    for (; j + 63 < m; j += 64) {
+                        __m256i orix = _mm256_loadu_si256((const __m256i *) (a + j / 2));
+                        __m512i by = _mm512_loadu_si512((const __m512i *) (b + j));
+
+                        __m512i bytex = _mm512_inserti64x4(_mm512_castsi256_si512(orix), _mm256_srli_epi16(orix, 4), 1);
+                        __m512i bx = _mm512_and_si512(lowMask, bytex);
+                        acc = _mm512_dpbusd_epi32(acc, by, bx);
+                    }
+
+                    values[block * k + i] = _mm512_reduce_add_epi32(acc);
+                }
+            }
+*/
+            block = 0;
+            for (; block + 3 < n; block += 4) {
+                uint8_t *weightWalk = b;
+                uint8_t *inputStart = a + block * m;
+
+                for (int i = 0; i < k; i++) {
+                    uint8_t *a = weightWalk + (i * m) / 2;
+                    uint8_t *b = inputStart;
+
+                    __m256i acc0 = _mm256_setzero_si256();
+                    __m256i acc1 = _mm256_setzero_si256();
+                    __m256i acc2 = _mm256_setzero_si256();
+                    __m256i acc3 = _mm256_setzero_si256();
+
+                    const __m256i lowMask = _mm256_set1_epi8(0xf);
+                    const __m256i ones = _mm256_set1_epi16(1);
+                    int j = 0, ans = 0;
+                    for (; j + 31 < m; j += 32) {
+                        __m128i orix = _mm_loadu_si128((const __m128i *) (a + j / 2));
+                        __m256i bytex = _mm256_set_m128i(_mm_srli_epi16(orix, 4), orix);
+                        __m256i bx = _mm256_and_si256(lowMask, bytex);
+                        __m256i by0 = _mm256_loadu_si256((const __m256i *) (b + j));
+                        __m256i by1 = _mm256_loadu_si256((const __m256i *) (b + m * 1 + j));
+                        __m256i by2 = _mm256_loadu_si256((const __m256i *) (b + m * 2 + j));
+                        __m256i by3 = _mm256_loadu_si256((const __m256i *) (b + m * 3 + j));
+
+                        acc0 = _mm256_add_epi32(acc0, _mm256_madd_epi16(_mm256_maddubs_epi16(by0, bx), ones));
+                        acc1 = _mm256_add_epi32(acc1, _mm256_madd_epi16(_mm256_maddubs_epi16(by1, bx), ones));
+                        acc2 = _mm256_add_epi32(acc2, _mm256_madd_epi16(_mm256_maddubs_epi16(by2, bx), ones));
+                        acc3 = _mm256_add_epi32(acc3, _mm256_madd_epi16(_mm256_maddubs_epi16(by3, bx), ones));
+                    }
+                    values[block * k + i] = I32sum(acc0);
+                    values[(block + 1) * k + i] = I32sum(acc1);
+                    values[(block + 2) * k + i] = I32sum(acc2);
+                    values[(block + 3) * k + i] = I32sum(acc3);
+                }
+            }
+
+            for (; block + 2 < n; block += 3) {
+                uint8_t *weightWalk = b;
+                uint8_t *inputStart = a + block * m;
+
+                for (int i = 0; i < k; i++) {
+                    uint8_t *a = weightWalk + (i * m) / 2;
+                    uint8_t *b = inputStart;
+
+                    __m256i acc0 = _mm256_setzero_si256();
+                    __m256i acc1 = _mm256_setzero_si256();
+                    __m256i acc2 = _mm256_setzero_si256();
+
+                    const __m256i lowMask = _mm256_set1_epi8(0xf);
+                    const __m256i ones = _mm256_set1_epi16(1);
+                    int j = 0, ans = 0;
+                    for (; j + 31 < m; j += 32) {
+                        __m128i orix = _mm_loadu_si128((const __m128i *) (a + j / 2));
+                        __m256i bytex = _mm256_set_m128i(_mm_srli_epi16(orix, 4), orix);
+                        __m256i bx = _mm256_and_si256(lowMask, bytex);
+                        __m256i by0 = _mm256_loadu_si256((const __m256i *) (b + j));
+                        __m256i by1 = _mm256_loadu_si256((const __m256i *) (b + m * 1 + j));
+                        __m256i by2 = _mm256_loadu_si256((const __m256i *) (b + m * 2 + j));                        
+
+                        acc0 = _mm256_add_epi32(acc0, _mm256_madd_epi16(_mm256_maddubs_epi16(by0, bx), ones));
+                        acc1 = _mm256_add_epi32(acc1, _mm256_madd_epi16(_mm256_maddubs_epi16(by1, bx), ones));
+                        acc2 = _mm256_add_epi32(acc2, _mm256_madd_epi16(_mm256_maddubs_epi16(by2, bx), ones));
+                    }
+                    values[block * k + i] = I32sum(acc0);
+                    values[(block + 1) * k + i] = I32sum(acc1);
+                    values[(block + 2) * k + i] = I32sum(acc2);
+                }
+            }
+
+            for (; block + 1 < n; block += 2) {
+                uint8_t *weightWalk = b;
+                uint8_t *inputStart = a + block * m;
+
+                for (int i = 0; i < k; i++) {
+                    uint8_t *a = weightWalk + (i * m) / 2;
+                    uint8_t *b = inputStart;
+
+                    __m256i acc0 = _mm256_setzero_si256();
+                    __m256i acc1 = _mm256_setzero_si256();
+
+                    const __m256i lowMask = _mm256_set1_epi8(0xf);
+                    const __m256i ones = _mm256_set1_epi16(1);
+                    int j = 0, ans = 0;
+                    for (; j + 31 < m; j += 32) {
+                        __m128i orix = _mm_loadu_si128((const __m128i *) (a + j / 2));
+                        __m256i bytex = _mm256_set_m128i(_mm_srli_epi16(orix, 4), orix);
+                        __m256i bx = _mm256_and_si256(lowMask, bytex);
+                        __m256i by0 = _mm256_loadu_si256((const __m256i *) (b + j));
+                        __m256i by1 = _mm256_loadu_si256((const __m256i *) (b + m * 1 + j));
+
+                        acc0 = _mm256_add_epi32(acc0, _mm256_madd_epi16(_mm256_maddubs_epi16(by0, bx), ones));
+                        acc1 = _mm256_add_epi32(acc1, _mm256_madd_epi16(_mm256_maddubs_epi16(by1, bx), ones));
+                    }
+                    values[block * k + i] = I32sum(acc0);
+                    values[(block + 1) * k + i] = I32sum(acc1);
+                }
+            }
+
+            for (; block < n; block++) {
+                uint8_t *weightWalk = b;
+                uint8_t *inputStart = a + block * m;
+
+                for (int i = 0; i < k; i++) {
+                    uint8_t *a = weightWalk + (i * m) / 2;
+                    uint8_t *b = inputStart;
+
+                    __m256i acc = _mm256_setzero_si256();
+                    const __m256i lowMask = _mm256_set1_epi8(0xf);
+                    const __m256i ones = _mm256_set1_epi16(1);
+                    int j = 0, ans = 0;
+                    for (; j + 31 < m; j += 32) {
+                        __m128i orix = _mm_loadu_si128((const __m128i *) (a + j / 2));
+                        __m256i bytex = _mm256_set_m128i(_mm_srli_epi16(orix, 4), orix);
+                        __m256i bx = _mm256_and_si256(lowMask, bytex);
+                        __m256i by = _mm256_loadu_si256((const __m256i *) (b + j));
+                        acc = _mm256_add_epi32(acc, _mm256_madd_epi16(_mm256_maddubs_epi16(by, bx), ones));
+                    }
+                    values[block * k + i] = I32sum(acc);
+                }
+            }
+
+            block = 0;
+            for (; block < n; block++) {
+                int i = 0;
+                for (; i < k; i++) {
+                    const float vv = (float)values[block * k + i] - weightSums[i] * izeros[block];
+                    float sum = scales[i] * iscales[block] * vv + weightMins[i] * tempValue[block];
+                    ((float*)c)[block * kstride + i] = sum + (bias == nullptr ? 0.0 : bias[i]);
+                }
+            }
+            return;
+        }
+#endif
         std::vector <float> values;
         values.resize(group);
 
