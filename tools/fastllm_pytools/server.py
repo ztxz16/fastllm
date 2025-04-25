35ed898b098b7b270ad97420149f070d992f17fc
黄宇扬 <huang.yuyang@think-force.com>
2025-04-25 11:17:02 +0800

    增加参数隐藏server的input 信息

--- tools/fastllm_pytools/server.py
+++ tools/fastllm_pytools/server.py
@@ -111,7 +111,7 @@ def fastllm_server(args):
         args.model_name = args.path
         if (args.model_name is None or args.model_name == ''):
             args.model_name = args.model
-    fastllm_completion = FastLLmCompletion(model_name = args.model_name, model = model, think =  args.think)
+    fastllm_completion = FastLLmCompletion(model_name = args.model_name, model = model, think = args.think, hide_input = args.hide_input)
     fastllm_embed = FastLLmEmbed(model_name = args.model_name, model = model)
     fastllm_reranker = FastLLmReranker(model_name = args.model_name, model = model)
     fastllm_model = FastLLmModel(model_name = args.model_name)
