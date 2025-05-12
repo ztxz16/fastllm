d3c230f8bdff5b1ac9b402d273190f3cb03dc92e
黄宇扬 <410644548@qq.com>
2025-05-12 14:28:09 +0800

    提交生成rocm下whl的脚本

--- src/devices/cuda/fastllm-cuda.cu
+++ src/devices/cuda/fastllm-cuda.cu
@@ -1811,33 +1811,48 @@ template <int THREAD_PER_BLOCK, int PART>
 __global__ void FastllmGemvInt4GroupKernel2(float *A, uint8_t *B, float *C,
                                              float *bias, half *scales, half *mins,
                                              int m, int k, int group, int groupCnt) {
-    __shared__ float sdata[THREAD_PER_BLOCK];
+    __shared__ float sdata[PART][THREAD_PER_BLOCK];
     unsigned int tid = threadIdx.x;
     // 1. 计算
     int st = blockIdx.x * PART;
     int end = st + PART;
-    for (int p = st; p < end; p++) {
-        sdata[tid] = 0;
-        for (int i = tid; i < m / 4; i += THREAD_PER_BLOCK) {
-            uint8_t now0 = B[p * m / 2 + i * 2];
-            uint8_t now1 = B[p * m / 2 + i * 2 + 1];
-            float4 aBuffer = FETCH_FLOAT4(A[i * 4]);
-            int g = p * group + (i * 4 / groupCnt);
-            sdata[tid] += (aBuffer.x * ((float)mins[g] + (float)scales[g] * (now0 >> 4)) 
-                         + aBuffer.y * ((float)mins[g] + (float)scales[g] * (now0 & 15)));
-            sdata[tid] += (aBuffer.z * ((float)mins[g] + (float)scales[g] * (now1 >> 4)) 
-                         + aBuffer.w * ((float)mins[g] + (float)scales[g] * (now1 & 15)));
+    #pragma unroll
+    for (int p = 0; p < PART; p++) {
+        sdata[p][tid] = 0;
+    }
+
+    for (int i = tid; i < m / 8; i += THREAD_PER_BLOCK) {
+        float4 aBuffer = FETCH_FLOAT4(A[i * 8]);
+        float4 bBuffer = FETCH_FLOAT4(A[i * 8 + 4]);
+
+        for (int p = st; p < end; p++) {
+            uint8_t now0 = B[p * m / 2 + i * 4];
+            uint8_t now1 = B[p * m / 2 + i * 4 + 1];
+            uint8_t now2 = B[p * m / 2 + i * 4 + 2];
+            uint8_t now3 = B[p * m / 2 + i * 4 + 3];
+            int g = p * group + (i * 8 / groupCnt);
+            float curmin = (float)mins[g], curscale = (float)scales[g];
+            sdata[p - st][tid] += (aBuffer.x * (curmin + (float)curscale * (now0 >> 4)) 
+                         + aBuffer.y * (curmin + (float)curscale * (now0 & 15)));
+            sdata[p - st][tid] += (aBuffer.z * (curmin + (float)curscale * (now1 >> 4)) 
+                         + aBuffer.w * (curmin + (float)curscale * (now1 & 15)));
+            sdata[p - st][tid] += (bBuffer.x * (curmin + (float)curscale * (now2 >> 4)) 
+                         + bBuffer.y * (curmin + (float)curscale * (now2 & 15)));
+            sdata[p - st][tid] += (bBuffer.z * (curmin + (float)curscale * (now3 >> 4)) 
+                         + bBuffer.w * (curmin + (float)curscale * (now3 & 15)));
         }
-        __syncthreads();
+    }
+    __syncthreads();
+    for (int p = 0; p < PART; p++) {
         for (unsigned int s = 1; s < THREAD_PER_BLOCK; s *= 2) {
             if ((tid & (2 * s - 1)) == 0) {
-                sdata[tid] += sdata[tid + s];
+                sdata[p][tid] += sdata[p][tid + s];
             }
             __syncthreads();
         }
 
         if (tid == 0) {
-            C[p] = sdata[0] + bias[p];
+            C[st + p] = sdata[p][0] + bias[st + p];
         }
         __syncthreads();
     }
@@ -2934,7 +2949,7 @@ bool FastllmCudaMatMulFloatInt4(const fastllm::Data &input, fastllm::Data &weigh
 
 void LaunchFastllmGemmFp32Int4Group(float *input, uint8_t *weight, float *output, float *bias, half *scales, half *mins, int n, int m, int k, int group, int groupCnt) {
     for (int i = 0; i < n; i++) {
-        FastllmGemvInt4GroupKernel2<64, 1> <<< k, 64 >>>(input + i * m, weight, output + i * k, bias, scales, mins, m, k, group, groupCnt);
+        FastllmGemvInt4GroupKernel2<64, 4> <<< k / 4, 64 >>>(input + i * m, weight, output + i * k, bias, scales, mins, m, k, group, groupCnt);
     }
 }
 
