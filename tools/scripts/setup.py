7ba3a57921b1ab571a2a139c8e9a6abad702e3f1
黄宇扬 <410644548@qq.com>
2025-05-12 17:16:53 +0800

    V 0.0.1.1

--- tools/scripts/setup.py
+++ tools/scripts/setup.py
@@ -7,7 +7,7 @@ all_require = server_require + webui_require + download_require
 
 setup (
     name = "ftllm",
-    version = "0.0.1.0",
+    version = "0.0.1.1",
     author = "huangyuyang",
     author_email = "ztxz16@foxmail.com",
     description = "Fastllm",
