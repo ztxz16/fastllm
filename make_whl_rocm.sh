2c7407f5474fcff89ad7172a18fe96960ebffc0c
黄宇扬 <huang.yuyang@think-force.com>
2025-05-12 14:28:09 +0800

    提交生成rocm下whl的脚本

---
+++ make_whl_rocm.sh
@@ -0,0 +1,32 @@
+source ~/ftllm/bin/activate
+pip install setuptools wheel -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+
+#!/bin/bash
+folder="build-fastllm-rocm"
+
+# 创建工作文件夹
+rm -rf "$folder"
+mkdir "$folder"
+cd $folder
+
+# cpu
+rm -rf CMakeCache.txt CMakeFiles
+cmake .. -DMAKE_WHL_X86=ON -DUSE_ROCM=OFF -DUSE_NUMA=ON
+make fastllm_tools -j$(nproc)
+if [ $? != 0 ]; then
+    exit -1
+fi
+cp tools/ftllm/libfastllm_tools.so tools/ftllm/libfastllm_tools-cpu.so
+
+# cuda-11
+rm -rf CMakeCache.txt CMakeFiles
+cmake .. -DMAKE_WHL_X86=ON -DUSE_ROCM=ON -DUSE_NUMA=ON -DROCM_ARCH="gfx906;gfx908;gfx90a;gfx942;gfx1030;gfx1100;gfx1101"
+make fastllm_tools -j$(nproc)
+if [ $? != 0 ]; then
+    exit -1
+fi
+
+cd tools
+ldd ftllm/libfastllm_tools.so | grep '=>' | awk '{print $3}' | grep 'libnuma' | xargs -I {} cp -n {} ftllm/.
+python3 setup_rocm.py sdist build
+python3 setup_rocm.py bdist_wheel --plat-name manylinux2014_$(uname -m)
---
+++ tools/scripts/setup_rocm.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+server_require = ['fastapi', 'pydantic', 'openai', 'shortuuid', 'uvicorn']
+webui_require = ['streamlit-chat']
+download_require = ['aria2']
+all_require = server_require + webui_require + download_require
+
+setup (
+    name = "ftllm_rocm",
+    version = "0.0.1.0",
+    author = "huangyuyang",
+    author_email = "ztxz16@foxmail.com",
+    description = "Fastllm",
+    url = "https://github.com/ztxz16/fastllm",
+    entry_points = {
+        'console_scripts' : [
+            'ftllm=ftllm.cli:main'
+        ]
+    },
+    packages = ['ftllm', 'ftllm/openai_server', 'ftllm/openai_server/protocal'],
+    package_data = {
+        '': ['*.dll', '*.so', '*.dylib', '*.so.*']
+    },
+    install_requires=[
+        'pyreadline3',
+        'transformers',
+        'jinja2>=3.1.0'
+    ] + all_require,
+    extras_require={
+        'all': all_require,
+        'server': server_require,
+        'webui': webui_require
+    },
+)
