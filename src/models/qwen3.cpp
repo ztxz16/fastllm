274af456fb8331eb7da4958031e27250f915b6ad
黄宇扬 <410644548@qq.com>
2025-05-23 11:59:55 +0800

    multicuda mlp改写为更通用的形式，支持fp8并行

--- src/models/qwen3.cpp
+++ src/models/qwen3.cpp
@@ -302,7 +302,7 @@ namespace fastllm {
             std::string swigluWeightName = "model.layers." + std::to_string(i) + ".mlp.gateup_proj.weight";
             if (weight.weight.find(swigluWeightName) != weight.weight.end() && CanRunMLP()) {
                 std::string downWeightName = "model.layers." + std::to_string(i) + ".mlp.down_proj.weight";
-                MLP(attenInput, weight[swigluWeightName], Data(), weight[downWeightName], Data(), k);
+                MLP(attenInput, weight[swigluWeightName], Data(), weight[downWeightName], Data(), w1, w2, w3, k);
                 AddTo(hiddenStates, k);
             } else {
                 if (weight.weight.find(swigluWeightName) != weight.weight.end()) {
@@ -685,7 +685,7 @@ namespace fastllm {
             std::string swigluWeightName = "model.layers." + std::to_string(i) + ".mlp.gateup_proj.weight";
             if (weight.weight.find(swigluWeightName) != weight.weight.end() && CanRunMLP()) {
                 std::string downWeightName = "model.layers." + std::to_string(i) + ".mlp.down_proj.weight";
-                MLP(attenInput, weight[swigluWeightName], Data(), weight[downWeightName], Data(), k);
+                MLP(attenInput, weight[swigluWeightName], Data(), weight[downWeightName], Data(), w1, w2, w3, k);
                 AddTo(hiddenStates, k);
             } else {
                 if (weight.weight.find(swigluWeightName) != weight.weight.end()) {
