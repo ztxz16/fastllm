fa8a3b5371b996f19cb8f84c3f2ac194c8d1ab08
黄宇扬 <410644548@qq.com>
2025-09-08 17:31:04 +0800

    v0.1.4.5

--- tools/scripts/setup_rocm.py
+++ tools/scripts/setup_rocm.py
@@ -7,7 +7,7 @@ all_require = server_require + webui_require + download_require
 
 setup (
     name = "ftllm_rocm",
-    version = "0.1.4.3",
+    version = "0.1.4.5",
     author = "huangyuyang",
     author_email = "ztxz16@foxmail.com",
     description = "Fastllm",
