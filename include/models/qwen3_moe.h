d037a89f6c6cf450414b4e69cfc57eac439a8efa
黄宇扬 <410644548@qq.com>
2026-02-24 14:09:53 +0800

    qwen3_moe完全换用新方式

--- include/models/qwen3_moe.h
+++ include/models/qwen3_moe.h
@@ -28,27 +28,6 @@ namespace fastllm {
                 const GenerationConfig &generationConfig = GenerationConfig(),
                 const LastTokensManager &lastTokens = LastTokensManager(),
                 std::vector <float> *logits = nullptr);
-
-        std::vector <int> ForwardBatch(
-                int batch,
-                const Data &inputIds,
-                const Data &attentionMask,
-                const Data &positionIds,
-                std::vector <std::pair <Data, Data> > &pastKeyValues,
-                const GenerationConfig &generationConfig = GenerationConfig(),
-                const LastTokensManager &lastTokens = LastTokensManager(),
-                std::vector <std::vector <float>*> *logits = nullptr);
-
-        std::vector <int> ForwardBatch(
-                int batch,
-                const Data &inputIds,
-                const std::vector <Data*> &attentionMask,
-                const std::vector <Data*> &positionIds,
-                const std::vector <int> &seqLens,
-                std::vector <std::pair <Data*, Data*> > &pastKeyValues,
-                const std::vector <GenerationConfig> &generationConfigs,
-                const LastTokensManager &lastTokens = LastTokensManager(),
-                std::vector <std::vector <float>*> *logits = nullptr);
                 
         std::vector <int> ForwardV2(
                     int batch,
@@ -74,8 +53,6 @@ namespace fastllm {
         virtual std::string MakeInput(const std::string &history, int round, const std::string &input); // 根据历史信息和当前输入生成prompt
 
         virtual std::string MakeHistory(const std::string &history, int round, const std::string &input, const std::string &output); // 根据当前回复更新history
-
-        std::pair<std::vector<float>, std::vector<float>> UpdateRotaryPosEmb(float base, float factor, int seqLen = 0); // 更新位置编码
 
     protected:
         RoPEType rope_type = RoPEType::BASE;
